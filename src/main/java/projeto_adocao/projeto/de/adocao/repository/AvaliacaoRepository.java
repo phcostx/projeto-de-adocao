7559fa3875a8994be6a3cda60a1a531c57203bd6
phcostx <phcostx@hotmail.com>
2025-07-04 22:30:11 -0300

    feat: criacao das entendidades JPA e gerar tabelas no banco

--- src/main/java/projeto_adocao/projeto/de/adocao/repository/AvaliacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package projeto_adocao.projeto.de.adocao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projeto_adocao.projeto.de.adocao.model.Avaliacao;
-
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
-}
